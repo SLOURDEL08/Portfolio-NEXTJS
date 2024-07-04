c631ed5405463e919a6a1b0c3b568940b161cb07
SebLOURDEL <seb.lourdel8@gmail.com>
2024-07-04 23:20:25 +0200

    changes config files from #6fef5d2 from github

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './src/pages/**/*.{js,jsx,ts,tsx}',
-    './src/components/**/*.{js,jsx,ts,tsx}',
-    './src/app/**/*.{js,jsx,ts,tsx}',
-  ],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
-  plugins: [
-    'postcss-import',
-    'tailwindcss',
-    'autoprefixer',
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  },
 };
