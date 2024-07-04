09b29f47b8d1db8e7e2e6c6e6af4faab8863bc98
SebLOURDEL <seb.lourdel8@gmail.com>
2024-07-04 23:01:24 +0200

    postcss correction

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [
-    './pages/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}'
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/app/**/*.{js,jsx,ts,tsx}'
   ],
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 });
