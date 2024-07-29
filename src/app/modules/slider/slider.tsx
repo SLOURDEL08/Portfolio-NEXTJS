6947e08812d4795ad984628fbb9817c64743b788
SebLOURDEL <seb.lourdel8@gmail.com>
2024-07-29 09:20:05 +0200

    title homepage weight gap

--- src/app/modules/slider/slider.tsx
+++ src/app/modules/slider/slider.tsx
@@ -4,56 +4,37 @@ import clsx from 'clsx';
 
 const Slider = ({ className = '' }) => {
   return (
-    <div className={clsx("custom-slider", className)}>
-      <div className="custom-slide-track grayz ">
-        <div className="custom-slide">
-          <Image src="/slider/html.png" width="100" height="100" alt="" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/css.png" width="100" height="100" alt="" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/sass.png" width="100" height="100" alt="" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/tailwind.png" width="100" height="100" alt="" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/bootstrap.png" width="100" height="100" alt="" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/js.png" alt="" width="100" height="100" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/php.png" alt="" width="100" height="100" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/react.webp" alt="" width="100" height="100" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/nodejs.png" alt="" width="100" height="100" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/ts.png" alt="" width="100" height="100" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/mongo.png" alt="" width="100" height="100" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/sql.png" alt="" width="100" height="100" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/firebase.png" alt="" width="100" height="100" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/fig.png" alt="" width="100" height="100" />
-        </div>
-        <div className="custom-slide">
-          <Image src="/slider/photoshop.png" alt="" width="100" height="100" />
-        </div>
-        
-        
-       
+    <div className={clsx('carousel-container', className)}>
+      <div className='carousel-track'>
+        {[
+          'html',
+          'css',
+          'sass',
+          'tailwind',
+          'bootstrap',
+          'js',
+          'php',
+          'react',
+          'nodejs',
+          'ts',
+          'mongo',
+          'sql',
+          'firebase',
+          'fig',
+          'photoshop',
+        ].map((src) => (
+          <div key={src} className='carousel-slide'>
+            <div className='image-wrapper'>
+              <Image
+                src={`/slider/${src}.png`}
+                width='1000'
+                height='1000'
+                alt={src}
+                className='masked-image object-contain'
+              />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
