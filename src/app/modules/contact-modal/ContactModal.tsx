9ef3980aa854c04a5390668796c3a93cea3781ca
SebLOURDEL <seb.lourdel8@gmail.com>
2024-07-29 17:46:55 +0200

    form isMobile correction -551px width mediaqueries

--- src/app/modules/contact-modal/ContactModal.tsx
+++ src/app/modules/contact-modal/ContactModal.tsx
@@ -25,7 +25,7 @@ const getCustomStyles = (isMobile: boolean) => ({
     border: 'none',
     outline: 'none',
     width: '100%',
-    height: '60px', // Hauteur fixe
+    height: isMobile ? '50px' : '60px', // Hauteur fixe
     paddingLeft: isMobile ? '10px' : '20px',
     borderRadius: '0.5em',
     fontSize: isMobile ? '15px' : '18px',
@@ -40,14 +40,14 @@ const getCustomStyles = (isMobile: boolean) => ({
   }),
   valueContainer: (provided: any) => ({
     ...provided,
-    height: '60px', // Hauteur fixe
+    height: isMobile ? '50px' : '60px', // Hauteur fixe
     display: 'flex',
     alignItems: 'center', // Centrer verticalement le contenu
     padding: '0px', // Assurez-vous que le padding est 0
   }),
   indicatorsContainer: (provided: any) => ({
     ...provided,
-    height: '60px', // Hauteur fixe
+    height: isMobile ? '50px' : '60px', // Hauteur fixe
   }),
   menu: (provided: any) => ({
     ...provided,
@@ -157,7 +157,7 @@ const ContactModal: React.FC = () => {
           weight='medium'
           theme='white'
           fontFamily='ClashDisplay'
-          className='text-center mb-14 max-zmd:mb-8 max-zmd:text-4xl max-zmd:text-left'
+          className='text-center mb-14 max-zmd:mb-8 max-zmd:text-3xl max-zmd:text-left'
         >
           Discutons ensemble !
         </Typography>
